384114316e5363aff42221934632deccb23b51bd
Şeyma Günönü <80623252+seymagnn@users.noreply.github.com>
2025-10-21 22:16:26 +0300

    Update charts_for_all_models.R

--- counterfactuals_for_all_models/charts_for_all_models.R
+++ counterfactuals_for_all_models/charts_for_all_models.R
@@ -7,23 +7,7 @@ combined_datasets <- bind_rows(
   summary_metrics %>% mutate(Model = "Cost-sensitive")
 ) %>% mutate(Model = factor(Model, levels = c("Original", "Undersampling", "Oversampling", "SMOTE", "Cost-sensitive")))
 
-# making graph (4x5)
-ggplot(combined_datasets, aes(x = CE_Method, y = Mean_Values, color = Used_Model, group = Used_Model)) +
-  geom_line(position = position_dodge(width = 0.5)) +
-  geom_point(position = position_dodge(width = 0.5)) +
-  geom_errorbar(aes(ymin = Mean_Values - StdDev_Values, ymax = Mean_Values + StdDev_Values), width = 0.2,
-                position = position_dodge(width = 0.5)) +
-  facet_grid(Quality_Metric ~ Model, scales = "free_y") +
-  labs(x = "", y = "Mean Values", color = "Used Model") +
-  theme_minimal() +
-  theme(legend.position = "bottom") +
-  theme(legend.title=element_blank()) +
-  theme(axis.text.y = element_text(size = 13),
-        axis.text.x = element_text(size = 13),
-        axis.title.y = element_text(size = 13),
-        strip.text = element_text(size = 13))
-
-# 2
+# making graph
 ggplot(combined_datasets, aes(x = CE_Method, y = Mean_Values, color = Used_Model, group = Used_Model)) +
   geom_line(position = position_dodge(width = 0.5), linewidth = 1) +
   geom_point(size = 2.5, position = position_dodge(width = 0.5)) +
@@ -43,20 +27,3 @@ ggplot(combined_datasets, aes(x = CE_Method, y = Mean_Values, color = Used_Model
     panel.spacing = unit(0.8, "lines"),
     plot.margin = margin(10, 5, 10, 5)
   )
-
-
-# making graph (5x4)
-ggplot(combined_datasets, aes(x = CE_Method, y = Mean_Values, color = Used_Model, group = Used_Model)) +
-  geom_line(position = position_dodge(width = 0.5)) +
-  geom_point(position = position_dodge(width = 0.5)) +
-  geom_errorbar(aes(ymin = Mean_Values - StdDev_Values, ymax = Mean_Values + StdDev_Values), width = 0.2,
-                position = position_dodge(width = 0.5)) +
-  facet_grid(Model ~ Quality_Metric, scales = "free_x") +
-  labs(x = "", y = "Mean Values", color = "Used Model") +
-  theme_minimal() +
-  theme(legend.position = "bottom") +
-  theme(legend.title=element_blank()) +
-  theme(axis.text.y = element_text(size = 15),
-        axis.text.x = element_text(size = 15),
-        axis.title.y = element_text(size = 15),
-        strip.text = element_text(size = 13))
